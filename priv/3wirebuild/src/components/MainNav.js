f46eae35a89883ae66d697bdcd20cb2ffc6741a6
Matt Thompson <matthew.thompson.a@gmail.com>
2017-08-05 15:37:11 -0400

    settling on CSS-React Framework - Modern-Bul-Boot

---
+++ priv/3wirebuild/src/components/MainNav.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+
+export default class MainNav extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="faded" light toggleable>
+          <NavbarToggler right onClick={this.toggle} />
+          <NavbarBrand href="/">3WireBuild</NavbarBrand>
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/about/">About</NavLink>
+              </NavItem>
+
+              <NavItem>
+                <NavLink href="/projects/">Projects</NavLink>
+              </NavItem>
+
+              <NavItem>
+                <NavLink href="https://github.com/mthomps4">Github</NavLink>
+              </NavItem>
+
+              <NavItem>
+                <NavLink href="/blog/">Blog</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+Navbar.propTypes = {
+  light: PropTypes.bool,
+  inverse: PropTypes.bool,
+  full: PropTypes.bool,
+  fixed: PropTypes.string,
+  color: PropTypes.string,
+  role: PropTypes.string,
+  toggleable: PropTypes.oneOfType([PropTypes.bool, PropTypes.string]),
+  tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string])
+  // pass in custom element to use
+}
